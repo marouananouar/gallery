84744b0ff2a763db972512c7d93544f6eb844395
NinjaSQuad <lesaffaires8@gmail.com>
2024-04-14 20:48:15 +0100

    .

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2024, 6, 20, 8)}); // year, month, date, hour
+        });
+});		
+
